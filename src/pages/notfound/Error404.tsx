04bb400aea47b668b61676bf0b21fbf27d30a7b6
Kliti05 <kliti.hoxha@abp.al>
2024-10-23 13:53:34 +0200

    THG-156_Page-NotFound

--- src/pages/notfound/Error404.tsx
+++ src/pages/notfound/Error404.tsx
@@ -4,23 +4,23 @@ export default function Error404() {
   const navigate = useNavigate();
 
   return (
-    <div className="error-404 container-fluid small-offset-left">
-      <div className="logo"></div>
-      <div>
-        <h1 className="body-big__semi-bold">
-          4<span>0</span>4
-        </h1>
-        <h2 className="body-big__regular">Page Not Found</h2>
-        <p className="body-big__medium">
-          Sorry, the page you requested was not found! Please return to the
-          previous page!
-        </p>
+    <div className={"access-denied container-fluid small-offset-left"}>
+      <div className="m-auto">
+        <div className="no-access body-small__medium">
+          <span className="background-grey">404</span>
+          <span className="background-light-grey">Not Found</span>
+        </div>
+        <div className="access-denied__code boxed-regular__medium">Ups...</div>
       </div>
+      <h2 className="boxed-regular__semi-bold">
+        Es sieht so aus, als ob die angeforderte Seite nicht (mehr) existiert.
+      </h2>
       <button
-        className="button button--big button--blue"
+        className="button button-secondary button--big button--green"
         onClick={() => navigate(-1)}
       >
-        <span className="button__text">Go Back</span>
+        <i className="button_icon icon-arrow-left" />
+        <span className="button__text">Zurück zur Übersicht</span>
       </button>
     </div>
   );
