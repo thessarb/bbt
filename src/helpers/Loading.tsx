b68182ac284aa4ec7e09c418f984d1bddd038629
Kliti05 <kliti.hoxha@abp.al>
2024-10-28 16:51:09 +0100

    THG-167 Loading Component

---
+++ src/helpers/Loading.tsx
@@ -0,0 +1,12 @@
+const Loading = () => {
+  return (
+    <div className="loading-animation">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Loading;
