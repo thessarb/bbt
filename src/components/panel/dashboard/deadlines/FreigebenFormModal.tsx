073da31e5077748de851d0fa5c6e3c47b9a0a1ad
k.agaj <agaj@contenance.de>
2024-10-28 19:00:13 +0100

    THG-161 Plan-freigeben-form-modal

--- src/components/panel/dashboard/deadlines/FreigebenFormModal.tsx
+++ src/components/panel/dashboard/deadlines/FreigebenFormModal.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
+import PlanFreigebenForm from "./PlanFreigebenForm";
+import ListNoResult from "./ListNoResult";
 
 interface ThomasPlaneModalProps {
     showThomasModal: boolean;
@@ -17,22 +19,32 @@ function FreigebenFormModal({ showThomasModal, setShowThomasModal }: ThomasPlane
         }, 350);
     };
 
+    const handleFileUpload = () => {
+        alert("Upload file");
+    };
+
     return (
         <>
             <Modal
                 isOpen={showThomasModal}
                 toggle={handleClose}
-                className={`plan-view fade-in ${animateClose ? "fade-out" : ""}`}
+                className={`release-form fade-in ${animateClose ? "slide-up" : ""}`}
                 fade={true}
                 backdrop="static"
             >
                 <ModalHeader toggle={handleClose}>
                     <span className="heading__semibold">Auftrag 80700: Pläne ansehen</span>
                 </ModalHeader>
-                <ModalBody>thomas plane freigeben form modal</ModalBody>
+                <ModalBody>
+                    {PlanFreigebenForm ? < PlanFreigebenForm/> : <ListNoResult />}
+                </ModalBody>
                 <ModalFooter>
-                    <button className="button button-secondary button--big button--grey" onClick={handleClose}>
+                    <button className="button button-secondary button--big button--light-grey" onClick={handleClose}>
                         <span className="button__text">Schließen</span>
+                    </button>
+                    <button className="button button--big button--green" onClick={handleFileUpload}>
+                        <i className="button__icon icon-list-checks"></i>
+                        <span className="button__text">Plan freigeben</span>
                     </button>
                 </ModalFooter>
             </Modal>
