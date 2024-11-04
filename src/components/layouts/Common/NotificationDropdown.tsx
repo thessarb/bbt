bb4e4d274d34e0a8f1511792727e3156e8623602
Ariana <elezi@contenance.de>
2024-11-04 11:49:57 +0100

    THG-163 Fixes

--- src/components/layouts/Common/NotificationDropdown.tsx
+++ src/components/layouts/Common/NotificationDropdown.tsx
@@ -1,49 +1,47 @@
-import React, {Fragment, useState, useRef, useEffect} from "react";
-import {useNavigate} from "react-router";
+import React, { Fragment, useState, useRef, useEffect } from "react";
+import { useNavigate } from "react-router";
 import PATHS from "src/routes/Paths";
-
 interface Notification {
-    id: number;
-    text: string;
-    time: string;
+  id: number;
+  text: string;
+  time: string;
 }
-
 interface NotificationDropdownProps {
-    isOpen: boolean;
+  isOpen: boolean;
 }
 
 const NotificationDropdown: React.FC<NotificationDropdownProps> = ({
-                                                                       isOpen,
-                                                                   }) => {
-    const navigate = useNavigate();
-    const [toggleHandle, setToggleHandle] = useState<boolean>(false);
-    const [hasOpened, setHasOpened] = useState<boolean>(false)
-    const dropdownRef = useRef<HTMLDivElement | null>(null);
-    const [notificationsList, setNotificationsList] = useState<Notification[]>([
-        {
-            id: 1,
-            text: "Lieferung der Massivwand XY verzögert sich um X Tage.",
-            time: "2024-05-12",
-        },
-        {
-            id: 2,
-            text: "Ihre Lieferung des Bauabschnitts 2.1 kommt voraussichtlich am 03.05.2024",
-            time: "2024-05-12",
-        },
-    ]);
+  isOpen,
+}) => {
+  const navigate = useNavigate();
+  const [toggleHandle, setToggleHandle] = useState<boolean>(false);
+  const [hasOpened, setHasOpened] = useState<boolean>(false)
+  const dropdownRef = useRef<HTMLDivElement | null>(null);
+  const [notificationsList, setNotificationsList] = useState<Notification[]>([
+    {
+      id: 1,
+      text: "Lieferung der Massivwand XY verzögert sich um X Tage.",
+      time: "2024-05-12",
+    },
+    {
+      id: 2,
+      text: "Ihre Lieferung des Bauabschnitts 2.1 kommt voraussichtlich am 03.05.2024",
+      time: "2024-05-12",
+    },
+  ]);
 
-    const handleClickInside = (event: React.MouseEvent) => {
-        const isButtonClick = (event.target as HTMLElement).closest(
-            ".button-gost"
-        );
-        if (!isButtonClick) {
-            event.stopPropagation();
-        }
-    };
-
-    if (isOpen && !hasOpened) {
-        setHasOpened(true);
+  const handleClickInside = (event: React.MouseEvent) => {
+    const isButtonClick = (event.target as HTMLElement).closest(
+      ".button-gost"
+    );
+    if (!isButtonClick) {
+      event.stopPropagation();
     }
+  };
+
+  if (isOpen && !hasOpened) {
+    setHasOpened(true);
+  }
 
     return (
         <div
